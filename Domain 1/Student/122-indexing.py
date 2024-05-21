ab56e4ae93f09c0d4cd86c73f080eba646dd6f75
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-21 08:59:48 -0400

    Update 122-indexing.py

--- Domain 1/Student/122-indexing.py
+++ Domain 1/Student/122-indexing.py
@@ -1,3 +1,5 @@
 categories = ["Early Bronze Age", "Middle Bronze Age", "Late Bronze Age", "Bronze Age Collapse", "Early Iron Age"]
 print(categories)
 
+print(categories[-1])#To return the last item in the list
+print(categories[-2])#To return the second to last item in the list
