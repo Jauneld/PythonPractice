722a3772a341d7c78a258cf983261b1c944d8d03
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-21 09:10:32 -0400

    Update and rename 123-slicing.py to 123-slicing-completed.py

---
+++ Domain 1/Student/123-slicing-completed.py
@@ -0,0 +1,9 @@
+serial_number = "1111-2222-3333-4444"
+print(serial_number[:4])#the colon represent that we want to start slicing 
+#from the beginning of the string going for 4 characters
+print(serial_number[4:])#the colon represent that we want to start slicing 
+#after the 4th character
+print(serial_number[-4:])#Slice the 4 characters starting at the end of the text
+print(serial_number[5:14])#Slice in the middle of the list. Number before the colon is where to 
+#start. The Number after the colon is the number of the character where the slice will end
+#Just add the number of characters you want to print to the starting point to know where to end
