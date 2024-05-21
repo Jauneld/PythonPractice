fe040fa49a0d6ded0567ba43534f3844b668f91a
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-21 08:55:29 -0400

    Update 121-conversion-completed.py

--- Domain 1/Student/121-conversion.py
+++ Domain 1/Student/121-conversion.py
@@ -1,4 +1,5 @@
 # rating is supposed to be a whole number
-rating = (input("Enter a rating between 1 and 5"))
-points = (rating) * 100
-print("You have " + (points) + " to start.")
+#default data type for a variable is a string
+rating = float(input("Enter a rating between 1 and 5"))#converting data type from string to a float
+points = int(rating) * 100#converting data type from string to a integer
+print("You have " + str(points) + " to start.")#converting data type from integer to a string
