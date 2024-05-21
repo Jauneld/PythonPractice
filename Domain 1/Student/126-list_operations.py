7192107f5b705c098bd94e83ee2668d3a613bc65
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-21 09:30:14 -0400

    Update 126-list_operations.py

--- Domain 1/Student/126-list_operations.py
+++ Domain 1/Student/126-list_operations.py
@@ -1,4 +1,11 @@
+#a period(.) serves as the "assessor" that allows the programmer to use a method like a toolbox
+#methods are functions that are tied to a specific object and are invoked using the period notation
 capitals=["Montgomery","Juneau","Phoenix"]
+#object(toolbox).method(tool)("....")
+capitals.append("Sacramento")#add something to a list, at the end
+capitals.insert(3, "Little Rock")#to add an element to the list in a specific index
+#object(toolbox).method(tool)(index,"...")
 
+population = [200000,32000,1600000]
 
 print(capitals)
