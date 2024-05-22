8f8670503b10576a6c1587517a276246d0f07b2b
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 09:06:45 -0400

    Update and rename 135-identity.py to 135-identity-completed.py

---
+++ Domain 1/Student/135-identity-completed.py
@@ -0,0 +1,14 @@
+#The parentheses supersedes the operator 
+#*****Operator hierarchy*******
+#Arithmetic <--- Comparison <--- Identity <--- Logical <--- Assignment
+#Why first place? we need to solve the problem first
+#Identity: Determines if 2 variables share the same memory location so they are equal
+#identity keywords: "is" 
+
+team1 = {"color":"red", "rank":1}
+team2 = team1
+team3 = {"color":"red", "rank":1}
+
+print(team1 is team2)#True because Team2 has been made equal to Team1 so they share the same memory space
+print(team1 is team3)#False because Team3 is a new variable with its own values so it doesn't share the same memory space
+
