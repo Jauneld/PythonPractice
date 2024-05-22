9fdd1a9aebe50bbd93decc5fa41b982e816a842c
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 08:59:03 -0400

    Update and rename 134-arithmetic.py to 134-arithmetic-completed.py

---
+++ Domain 1/Student/134-arithmetic-completed.py
@@ -0,0 +1,11 @@
+#The parentheses supersedes the operator 
+#*****Operator hierarchy*******
+#Arithmetic <--- Comparison <--- Logical <--- Assignment
+#Why first place? we need to solve the problem first
+#Arithmetic: Remember PEMDAS
+base = 4000
+rank = 3
+bonus = 500
+
+total_score = (base + bonus) * rank
+print(total_score)
