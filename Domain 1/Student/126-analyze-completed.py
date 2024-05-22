d47e501e4d927780bf950dd176240ff92be05530
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 08:33:01 -0400

    Update and rename 126-analyze.py to 126-analyze-completed.py

---
+++ Domain 1/Student/126-analyze-completed.py
@@ -0,0 +1,8 @@
+quote = "The quick brown fox jumped over the lazy dog."
+print(quote[4:9])
+
+coins = ('Platinum','Gold','Silver') #What type of data structure is this?
+#Can we change Platinum to Bronze?
+#Yes because it is a tuple. In a tuple the list is unchangable unless you reassign the entire list to the variable
+coins = ('Bronze','Gold','Sliver')
+print(coins)
