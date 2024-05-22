4a72933750dfe7ee331c95ffd513d2f8ebe37627
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 08:45:11 -0400

    Update and rename 132-comparison.py to 132-comparison-completed.py

---
+++ Domain 1/Student/132-comparison-completed.py
@@ -0,0 +1,8 @@
+#The parentheses supersedes the operator 
+#Compares 2 values
+#Return True or False
+#They run left to right and if on part is False, the entire statement is False
+x = 5
+y = 3
+print (x == y)
+print (x <= y > 2)
