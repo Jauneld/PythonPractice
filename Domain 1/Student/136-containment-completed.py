98a3456018b15472e32b89674781e9a627178383
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 09:12:26 -0400

    Update and rename 136-containment.py to 136-containment-completed.py

---
+++ Domain 1/Student/136-containment-completed.py
@@ -0,0 +1,11 @@
+#The parentheses supersedes the operator 
+#*****Operator hierarchy*******
+#Arithmetic <--- Containment <--- Comparison <--- Identity <--- Logical <--- Assignment
+#Why first place? we need to solve the problem first
+#Containment is 2nd because it is not as specific as the other comparison types: Comparison, identity, logical
+#Containment Operator checks of if a value is contained within a list of values
+#it is used most for lists, sets, tuples and dictionaries 
+#Contaiment keyword: "in"
+large_islands = ["Hokkaido", "Honshu", "Shikoku", "Kyushu"]
+island1 = "Hokkaido"
+print(island1 in large_islands)#True because it is in the list large_islands
