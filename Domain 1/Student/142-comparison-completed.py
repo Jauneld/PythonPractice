1527f6a2d491a95bce108890ca4e0e250526ddf7
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 19:42:43 -0400

    Update and rename 142-comparison.py to 142-comparison-completed.py

---
+++ Domain 1/Student/142-comparison-completed.py
@@ -0,0 +1,19 @@
+#Symbols 
+#> :greater than
+#< :less than
+#== :equal to 
+#>= :greater than or equal to 
+#<= :less than or equal to 
+#!= :not equal to
+
+player1_score = 300
+player2_score = 400
+player3_score = 300
+
+print(player1_score == player2_score)#False
+print(player1_score == player3_score)#True
+print(player1_score != player2_score)#True
+print(player1_score < player3_score)#False
+print(player1_score > player2_score)#False
+print(player1_score >= player2_score)#False
+print(player1_score <= player2_score)#True
