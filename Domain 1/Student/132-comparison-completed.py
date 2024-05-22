8efa8c0e7ca67703c31236be252ad12da59fe70c
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 08:58:26 -0400

    Update 132-comparison-completed.py

--- Domain 1/Student/132-comparison-completed.py
+++ Domain 1/Student/132-comparison-completed.py
@@ -1,4 +1,7 @@
 #The parentheses supersedes the operator 
+#*****Operator hierarchy*******
+#Arithmetic <--- Comparison <--- Logical <--- Assignment
+#Why first place? we need to solve the problem first
 #Compares 2 values
 #Return True or False
 #They run left to right and if on part is False, the entire statement is False
