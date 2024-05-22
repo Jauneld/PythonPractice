347c5ba838d60db07bdb3dec264b614dd7dc5b2e
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 19:35:39 -0400

    Update and rename 141-assignment.py to 141-assignment-completed.py

---
+++ Domain 1/Student/141-assignment-completed.py
@@ -0,0 +1,36 @@
+score = 300#simple assignment 
+
+score += 10 #compound assignment: changes the value of the variable with an arithmetic operator while storing it
+#score = score + 10
+print(score)
+
+score -= 10
+#score = score - 10
+print(score)
+
+score *= 10
+#score = score * 10
+print(score)
+
+score /= 10
+#score = score / 10
+print(score)
+
+score %= 10
+#score = score / 10 #remainder
+print(score)
+
+score = 300
+score //= 11
+#score = score / 10 #whole number
+print(score)
+
+score **=2
+#score = score^2
+print(score)
+
+x = y = 2
+<--------
+#Read from right to left
+y = 2
+x = y += 2
