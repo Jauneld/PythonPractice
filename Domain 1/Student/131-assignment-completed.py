18a244d72ec89bbf0dccddf63314ee73d4359e49
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 08:42:21 -0400

    Update and rename 131-assignment.py to 131-assignment-completed.py

---
+++ Domain 1/Student/131-assignment-completed.py
@@ -0,0 +1,8 @@
+#The parentheses supersedes the operator 
+x = 5
+y = 3
+print(x, y)
+#it always takes the latest value
+#the variable on the left side of the assignment operator (=) is set to teh value on the right
+y = x
+print(y)
