70261c324f9ce422133f979927255d67ebf55165
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 08:44:10 -0400

    Update and rename 214-nested.py to 214-nested-completed.py

---
+++ Domain 2/Student/214-nested-completed.py
@@ -0,0 +1,15 @@
+#Defined variables
+coins = ['Bronze', 'Silver', 'Gold','Platinum']
+coin = 'Bronze'
+score = 10000
+
+#nested if statement
+if score > 10000:#is the score greater than 10000
+    if coin in ('Gold','Platinum'):#if true, is the coin that the player has in the list 
+        print("You have reached level 3")#if true, the player is at level 3
+    else:
+        print("You have reached level 2")#if false, the player is at level 2
+elif score > 5000 and coin in coins:#if the inital if statement was false, if score is greater than 5000 and the coin that the playe has in the list coins
+    print("You have reached level 1. Keep going")#if true, the player is at level 1
+else:#if everything is false
+    print("Increase your score and collect coins to move up")#this will print 
