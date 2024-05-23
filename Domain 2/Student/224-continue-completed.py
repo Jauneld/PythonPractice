286aea7ccfd5627a6e45b9ed7e6f7d5556b4a9bf
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 09:33:59 -0400

    Update and rename 224-continue.py to 224-continue-completed.py

---
+++ Domain 2/Student/224-continue-completed.py
@@ -0,0 +1,6 @@
+coins = ('Bronze','Silver','Platinum','Gold')
+for coin in coins:
+    if coin == 'Platinum':
+        print('Congratulations! The platinum coin will move you to the next level!')
+        continue#skips an iteration of a loop
+    print ('You possess a', coin, 'coin.')
