d7402644391f209a6660f6b3517f8c4439616c1d
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 20:13:49 -0400

    Update and rename 146-containment.py to 146-containment-completed.py

---
+++ Domain 1/Student/146-containment-completed.py
@@ -0,0 +1,11 @@
+#Important to know if a value is located some where 
+#Keyword
+#"in": is the value asked for in the data type
+#"not in": is the value not in the data type
+
+ancient_civilizations = ['Mesopotamia','Egypt','Indus Valley','China']
+
+print('Egypt' in ancient_civilizations)
+print('Japan' in ancient_civilizations)
+print('Japan' not in ancient_civilizations)
+print('China' in ancient_civilizations)
