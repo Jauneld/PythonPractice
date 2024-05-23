17c4ada22e19424772db2749bd65d67ec609802a
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 08:26:48 -0400

    Update and rename 211-if.py to 211-if-completed.py

---
+++ Domain 2/Student/211-if-completed.py
@@ -0,0 +1,5 @@
+score = 8000
+
+if score > 10000:#colon shows the end of the condition
+    print("You have reached level 2")#action to perform if the condition is true
+print("Thank you for playing")
