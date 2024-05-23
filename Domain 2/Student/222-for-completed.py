fa489fb92d61cd65d3dcd0cc0d47034485876300
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 09:25:44 -0400

    Update and rename 222-for.py to 222-for-completed.py

---
+++ Domain 2/Student/222-for-completed.py
@@ -0,0 +1,8 @@
+#A for lopp is designed to iterate over a sequence, collection, or data structure, such as a list, tuple or string
+#it runs for a predetermined number of iterations based on the length of the collection 
+positions = 10
+for p in range(1,10+1):
+    print ("You are in position", p)
+
+for m in range(1,27):
+    print("You have reached mile", m)
