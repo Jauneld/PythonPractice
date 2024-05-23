c1e174ada78382aed0aa1fd8b1cb76ca40dba988
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 08:36:04 -0400

    Update and rename 213-else.py to 213-else-completed.py

---
+++ Domain 2/Student/213-else-completed.py
@@ -0,0 +1,11 @@
+#"else": allows the code to run is the "if" condition is not true and none of the "elif" conditions are true
+score = 4000
+
+if score > 10000:
+    print("You have reached level 2")#action for teh if statemnet that will be displayed
+elif score > 5000:#if the if statement is false 
+    print("You have reached level 1")#action for the elif statement that will be displayed
+else:#if none of the conditions work 
+    print("You need some work :) lol")#action that will be displayed
+
+print("Thanks for playing")#Out of conditional line of code 
