9560e9b9c25b54a160f070c19121dd6d78b1f693
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 09:30:52 -0400

    Update and rename 223-break.py to 223-break-completed.py

---
+++ Domain 2/Student/223-break-completed.py
@@ -0,0 +1,8 @@
+#Within loops, there may be a time in which you need to have a condition for exiting a loop early
+coins = ('Bronze','Silver','Platinum','Gold')
+for coin in coins:
+    print ('You possess a', coin, 'coin.')
+    if coin == 'Platinum':
+        print('Congratulations! You move to the next level!')
+        break#break exits the loop
+        
