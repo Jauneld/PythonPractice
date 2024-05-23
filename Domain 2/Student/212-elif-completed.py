188fc8ab9b44db4eb889d9011794b0a15c88768b
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 08:30:05 -0400

    Update and rename 212-elif.py to 212-elif-completed.py

---
+++ Domain 2/Student/212-elif-completed.py
@@ -0,0 +1,6 @@
+score = 8000
+
+if score > 10000:#looks if the score is over 10000
+    print("You have reached level 2")
+elif score > 5000:#looks if score is over 5000 if the first statement is false
+    print("You have reached level 1")
