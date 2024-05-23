f6aaf8a6bc53886d3f066c1df9d0605a8e161f26
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-22 20:04:03 -0400

    Update and rename 144-arithmetic.py to 144-arithmetic-completed.py

---
+++ Domain 1/Student/144-arithmetic-completed.py
@@ -0,0 +1,24 @@
+x = 5
+y = -3
+
+print (x + y)
+
+print (x - y)
+
+print (x * y)
+
+print (x / y)
+
+print (x // y)#Floor divison: returns the whole# portion of the answer
+
+print (x % y)#Modulus: shows the remainder of the divison
+
+print (x ** y)#Exponent: to the power of
+
+#Use PEMDAS
+print(x + 2 * y)
+print((x + 2) * y)
+
+print(-x)#Turn a positive # negative 
+print(-y)#Turn a negative # positive
+
