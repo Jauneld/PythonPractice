bf899c9b25a6900a8930e71941a8a0f023282baa
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-23 08:55:18 -0400

    Update and rename 221-while.py to 221-while-completed.py

---
+++ Domain 2/Student/221-while-completed.py
@@ -0,0 +1,9 @@
+#while loops: runs while True and stops when the statment becomes False
+score = 0
+score_increase = 10
+
+while score < 100:
+   print("You have", score, "points.") #part of the loop
+   score += score_increase #part of the loop
+
+print("You have reached 100 points. Congrats!")
