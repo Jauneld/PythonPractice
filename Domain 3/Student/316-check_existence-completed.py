4cf3976d4497a9c783545b4f4c7839985431669d
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 09:14:20 -0400

    Update and rename 316-check_existence.py to 316-check_existence-completed.py

---
+++ Domain 3/Student/316-check_existence-completed.py
@@ -0,0 +1,13 @@
+import os#libraries add bulit in functions that we can uses to complete tasks within our code
+
+if not os.path.exists('316-message.txt'):#exists is a method of the path function that is in the os library 
+    message = open('316-message.txt','w')
+    message.write('Testing file for player configuration\n')
+    message.write('Testing file for player score')
+    print("Configuration file made")
+    message.close()
+else: 
+    message_test = open('316-message.txt','r')
+    content = message_test.read()
+    print(content)
+    message_test.close()
