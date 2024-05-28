cc944f6f7d619b0182e0ff61c018222244063b4b
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 09:30:44 -0400

    Update and rename 318-analyze.py to 318-analyze-completed.py

---
+++ Domain 3/Student/318-analyze-completed.py
@@ -0,0 +1,12 @@
+#message = open('318-message.txt','w')
+#message.write('Testing file for player configuration\n')
+#message.write('Testing file for player score\n')
+#message.close()
+
+with open('318-message.text','w+') as message:#change w to w+ so the file is readable and writeable
+    #w is the primary opeartion in w+ so it will wipe out the existing content before reading the file
+    #if you want the computer to read and write to an existing file w/o deleting its content before writing use r+ instead
+    message.write('Testing file for player configuration\n')
+    message.write('Testing file for player score\n')
+    print('File created')
+    print(message.read())
