519ac60ec33daa0892f6ccd24c7773717388ab39
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 08:39:14 -0400

    Update and rename 227-analyze.py to 227-analyze-completed.py

---
+++ Domain 2/Student/227-analyze-completed.py
@@ -0,0 +1,11 @@
+operands = ['+','-','*','/','//','%']
+for operand in operands:
+    if operand == '//':
+        print('// is a floor division operand')
+        break
+        #continue
+    
+    print(operand, 'is an operand')
+
+#6 prints will run
+#5 print statements will run becauee the if loop was ended when the print statement was there
