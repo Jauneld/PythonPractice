d37c3e3899aed384596bb70435c6e9efa14ce833
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 08:56:56 -0400

    Update and rename 313-read.py to 313-read-completed.py

---
+++ Domain 3/Student/313-read-completed.py
@@ -0,0 +1,10 @@
+message = open('313-message.txt','w')# w: write mode
+message.write('Testing file for player configuration')
+message.write('Testing file for player score')
+message.close()
+
+message_test = open('313-message.txt','r')#opens the files(argument 1) in read mode(argument 2)
+#r+: read and write mode
+content = message_test.read()# a variable is requires to store the contents of the file being read
+print(content)# to 
+message_test.close()
