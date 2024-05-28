13798808bfdc8ede7c14276bdfc563c5fa2cb494
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 08:18:45 -0400

    Update and rename 225-pass.py to 225-pass-completed.py

---
+++ Domain 2/Student/225-pass-completed.py
@@ -0,0 +1,8 @@
+#pass: it doesn't break out of a loop or pause a loop. it pass a set of code
+coins = ('Bronze','Silver','Platinum','Gold')
+for coin in coins:
+    
+    if coin == 'Platinum':
+        pass# allows people to bulid parts of code without info and continue to structure code w/o waiting
+        continue
+    print ('You possess a', coin, 'coin.')
