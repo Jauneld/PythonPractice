1b31169120e6613eed9f647b6e26cc3da27c8904
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-05-28 21:40:45 -0400

    Update 525-analyze-completed.py

--- Domain 5/Student/525-analyze.py
+++ Domain 5/Student/525-analyze.py
@@ -1,10 +1,10 @@
 coins = 5
 games = 0
-try:
+try:#cant run by its self w/o except and finally
     result = coins/games
-except:
+except:# to catch execeptions. only runs if the try causes a runtime error
     print('This did not work. Did you to divide by zero?')
-else:
+else:#if there is a correct value. runs if try is succesful. except block is needed for the else block
     print('You are averaging', coins/games, 'coins per game.')
-finally:
+finally:#run reguardless
     print('Thank you for playing')
