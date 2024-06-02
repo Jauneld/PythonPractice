ebc254692111c67ca3a6d719f356af710f26b45c
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-06-02 15:51:02 -0400

    Update 621-math-completed.py

--- Domain 6/Student/621-math-completed.py
+++ Domain 6/Student/621-math-completed.py
@@ -1,19 +1,76 @@
 import math
 
-x=3.33
-y=-13
+#(nan) is a result not a method
 
-print(math.fabs(y))#absolute value[floating absolute]
+x=3.0
+y=-13.3
 
-print(math.ceil(x))#returns the next interger up on the number line after a decimal
+print(math.fabs(y))
+"""
+absolute value[floating absolute]
+"""
 
-print(math.floor(x))#returns an integer w/o a decimal number
+print(math.ceil(x))
+"""
+returns the next interger up on the number line after a decimal
+"""
 
-print(math.trunc(x))#returns the next highest positive number [towards 0]
+print(math.floor(x))
+"""
+returns an integer w/o a decimal number
+"""
 
-print(math.trunc(y))#returns the next lowest negative number [towards 0]
+print(math.trunc(x))
+"""
+returns the next highest positive number [towards 0]
+"""
 
-print(math.fmod(y,x))#returns the modulus from divided 2 numbers 
+print(math.trunc(y))
+"""
+returns the next lowest negative number [towards 0]
+"""
 
-print(math.frexp(x))#returns the mantissa, the fractional part of a log and the 
-#exponent of a number
+print(math.fmod(y,x))
+"""
+returns the modulus from divided 2 numbers
+"""
+
+print(math.frexp(x))
+"""
+returns the mantissa, the fractional part of a log and the 
+exponent of a number
+"""
+
+print(math.isnan(x))
+"""
+checks to see if a variable or expression is not a number
+and returns true is the expression is not a number and false if the expression
+is a number
+"""
+
+print(x*y)
+
+print(math.sqrt(x))
+"""
+returns the square root of a number
+"""
+
+print(math.isqrt(x))
+"""
+returns an integer of a square root of a number
+"""
+
+print(math.pow(y,x))
+"""
+raise the 1nd number to the of the 2nd number
+"""
+
+print(math.pi)
+"""
+returns pi
+"""
+
+print(math.pi * math.pow(x,2))
+"""
+radius of a circle
+"""
