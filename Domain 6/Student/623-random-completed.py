fdc5327c6fdbc6f60b9ab2e1808f72484c812e33
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-06-02 16:26:50 -0400

    Update and rename 623-random.py to 623-random-completed.py

---
+++ Domain 6/Student/623-random-completed.py
@@ -0,0 +1,7 @@
+from random 
+for i in range(10):
+    print(randrange(6,20,2))#returns a number between the first number and the last number - 1 
+    print(randint(1,20))#returns a number between the first number and the last number
+    x = random()#return a floating point decimal between 0 - 1
+    if x >= .75:
+        print("Bonus")
