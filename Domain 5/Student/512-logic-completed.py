6c8d272c423b16d545cf2ac16af7370a4b9ac59f
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-06-02 12:20:40 -0400

    Update and rename 512-logic.py to 512-logic-completed.py

---
+++ Domain 5/Student/512-logic-completed.py
@@ -0,0 +1,8 @@
+#logic error: The code runs but not in the way that is wanted(hard to spot)
+#normally, wrong order or code getting the wrong info
+score = 3000
+bonus = score/10 #300 on this test
+multiplier = 2
+
+print(score + bonus * multiplier) #expecting 6600
+#arthemitic problem: PEMDAS
