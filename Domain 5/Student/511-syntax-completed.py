e0da2bae1cfca861289961d93e1ef5021baaf141
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-06-02 12:16:37 -0400

    Update and rename 511-syntax.py to 511-syntax-completed.py

---
+++ Domain 5/Student/511-syntax-completed.py
@@ -0,0 +1,9 @@
+#syntax errors: punctuation and spelling errors in writing code. Code can't run
+
+# build a function to multiply a score and a multiplier
+def calculate_score(score, multiplier): #added colon
+    print('score upcoming')
+    return(score * multiplier)#incorrect indent
+    
+print(calculate_score(3000,2))#missing parentheses
+print(calculate_score(3200,1.5))
