3fe4ee38e2ee657a34fd0d2c53991cf7c9309eea
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-06-02 12:39:25 -0400

    Update and rename 513-runtime.py to 513-runtime-completed.py

---
+++ Domain 5/Student/513-runtime-completed.py
@@ -0,0 +1,8 @@
+"""runtime error: Happens when one tries to run code. They happen when one tries
+to do with an app what cannot be done as the app's code is being run. it can
+stem from LOGIC ERRORS. They cause various different types of error codes. 
+"""
+print(__doc__)
+plant_types = ['angiosperms','ferns','mosses']
+print(plant_types[0])
+print(plant_types[3])
